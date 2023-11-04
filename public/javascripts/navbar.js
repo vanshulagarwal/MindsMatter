3d7ea6bbf2d047f33b8c3915e3ec495b2d07d2e6
Yash <yashagarwal827327@gmail.com>
2023-11-05 03:38:33 +0530

    login page css updated

---
+++ public/javascripts/navbar.js
@@ -0,0 +1,37 @@
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+}
+
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach((n) => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
+}
+var bookedEvents = [];
+
+function bookSession(eventId) {
+  var eventElement = document.getElementById(eventId);
+  var eventName = eventElement.querySelector("h2").innerText;
+  var eventDate = eventElement.querySelector("p").innerText;
+
+  var confirmationElement = document.getElementById("confirmation");
+  confirmationElement.innerHTML = `Session booked for ${eventName} on ${eventDate}.`;
+  confirmationElement.style.display = "block";
+
+  bookedEvents.push({ name: eventName, date: eventDate });
+  var bookedEventsElement = document.getElementById("bookedEvents");
+  bookedEventsElement.innerHTML = "";
+  for (var i = 0; i < bookedEvents.length; i++) {
+    var event = bookedEvents[i];
+    bookedEventsElement.innerHTML += `<p>${event.name} - ${event.date}</p>`;
+  }
+}
